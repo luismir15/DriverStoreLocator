ad9845d77e55a2405a3d065403d4c023cbc412e3
luie <luie@MacBook-Pro.local>
2022-07-19 17:57:38 -0400

    spring project created

---
+++ src/main/java/com/luismir/kafka/driverlocator/DriverlocatorApplication.java
@@ -0,0 +1,13 @@
+package com.luismir.kafka.driverlocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverlocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverlocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luismir/kafka/driverlocator/DriverlocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luismir.kafka.driverlocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverlocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
