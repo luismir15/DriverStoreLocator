296330b5e79cfa2a8d9661938548b6bba6bab413
Luis Miranda <41742635+luismir15@users.noreply.github.com>
2022-08-10 17:56:18 -0400

    update

--- service/src/main/java/com/luismir/kafka/StoreServiceApplication.java
+++ service/src/main/java/com/luismir/kafka/StoreServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.example.storeservice.model")
+@EntityScan("com.luismir.kafka.model")
 public class StoreServiceApplication {
 
     public static void main(String[] args) {
